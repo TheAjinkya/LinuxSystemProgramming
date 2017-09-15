fc23a9e08d96e35472949a4cf66637e84d4954ca
ajinkya.chanshetty <ajinkya.chanshetty@gmail.com>
2017-09-15 16:55:31 +0530

    LSP programs

---
+++ 1/openFile.c
@@ -0,0 +1,24 @@
+#include "customopen.h"
+int main(int argc, char *argv[]){
+	int fileDesc;
+
+	validateNumberOfArguement(&argc);
+
+	fileDesc = open(argv[1],O_RDONLY);
+	if(fileDesc == -1){
+		perror("PERROR::open failed");
+	}
+	else{
+		printf("File is opened with FileDescriptor = %d\n",fileDesc);
+	}
+	close(fileDesc);
+
+	return 0;
+}
+
+void validateNumberOfArguement(const int *input){
+	if(*input != 2)
+	{
+		printf("Error : Not sufficient arguments\n Passed arguments %d",*input);
+	}
+}
